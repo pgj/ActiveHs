b63b44f1c4cacce22912d8a562aeec657df2c630
Gabor Pali <pali.gabor@gmail.com>
2012-04-26 20:25:13 +0200

    Make version derived from the Cabal package, bump copyright year

--- Args.hs
+++ Args.hs
@@ -7,6 +7,8 @@ import System.Directory (createDirectoryIfMissing)
 import System.Console.CmdArgs.Implicit
 import System.FilePath
 
+import Data.Version
+
 ------------------
 
 data Args
@@ -56,7 +58,7 @@ myargs = Args
         , verboseinterpreter = False                &= help "Verbose interpreter output in the browser"
         , recompilecmd  = "ghc -O" &= typ "COMMAND" &= help "Command to run before page generation. Default is 'ghc -O'."
         , magicname    = "a9xYf"  &= typ "VARNAME"  &= help "Magic variable name."
-        }  &= summary "activehs 0.2, (C) 2010-2011 Péter Diviánszky"
+        }  &= summary ("activehs " ++ showVersion version ++ ", (C) 2010-2012 Péter Diviánszky")
            &= program "activehs"
 
 completeArgs :: Args -> IO Args
