d92d1d6d2ac245f087c2e6ccbe2aa6490d84ea0c
Péter Diviánszky <divipp@gmail.com>
2013-02-12 16:27:58 +0100

    adjust default verbosity level

--- Args.hs
+++ Args.hs
@@ -54,7 +54,7 @@ myargs = Args
         , lang          = "en"    &= typ "LANGUAGE" &= help "Default language. It is 'en' by default."
         , port          = 8000    &= typ "PORT"     &= help "Port to listen"
         , static        = False                     &= help "Do not regenerate pages."
-        , verbose       = 0                         &= help "Verbose activehs output"
+        , verbose       = 2                         &= help "Verbose activehs output"
         , verboseinterpreter = False                &= help "Verbose interpreter output in the browser"
         , recompilecmd  = "ghc -O" &= typ "COMMAND" &= help "Command to run before page generation. Default is 'ghc -O'."
         , magicname    = "a9xYf"  &= typ "VARNAME"  &= help "Magic variable name."
